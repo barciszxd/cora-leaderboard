a19df7b60cfaf65fe453ddfb7778a6ebd9ec4836
barciszxd <marciniec.b@gmail.com>
2025-07-27 17:17:04 +0200

    Add endpoint to retrieve all athletes with gender information

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -226,6 +226,24 @@ def webhook():
     return jsonify({"success": False, "error": "Unsupported object type"}), 400
 
 
+@api_bp.route('/athletes', methods=['GET'])
+def get_athletes():
+    """Get all athletes"""
+    athlete_repo = athlete_service.AthleteRepository(db_session)
+    athletes = athlete_repo.get_all()
+
+    if not athletes:
+        return jsonify({"success": False, "error": "No athletes found"}), 404
+
+    # Convert athletes to a list of dictionaries
+
+    return jsonify([{
+        "id": athlete.id,
+        "name": f"{athlete.firstname} {athlete.lastname}",
+        "gender": athlete.sex,
+    } for athlete in athletes]), 200
+
+
 @api_bp.route('/challenges', methods=['POST'])
 def create_challenge():
     """Create a new challenge"""
